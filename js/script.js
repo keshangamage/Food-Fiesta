e62d7454bc44fbd2e8a16138989f082a41f3e5c3
Keshan Gamage <keshan.gimhana.gamage@gmail.com>
2025-04-27 19:44:33 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.getElementById('reservation-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  alert('Thank you for booking with Golden Fork! We look forward to serving you.');
+  alert('Thank you for booking with Food Fiesta! We look forward to serving you.');
 });
 
 const hamburger = document.querySelector('.hamburger');
